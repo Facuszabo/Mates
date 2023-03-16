33249cb4884957a5fcef8b9f1be133f8bf704d70
Facuszabo <facundoszabo6@gmail.com>
2023-03-15 23:54:19 -0300

    creada pagina home

--- materos/src/index.js
+++ materos/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Nav from './component/nav';
-import Slider from './component/slider';
-import Envios from './component/envios';
-
+import Home from './page/Home';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
-    <Nav />
-    <Slider/>
-    <Envios/>
-  </>
-)
+  <React.StrictMode>
+    <>
+    <Home/>
+    </>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
---
+++ materos/src/page/Home.jsx
@@ -0,0 +1,14 @@
+import Nav from '../component/nav';
+import Slider from '../component/slider';
+import Envios from '../component/envios';
+
+function Home(){
+  return(
+    <>
+    <Nav/>
+    <Slider/>
+    <Envios/>
+    </>
+  )
+}
+export default Home;
